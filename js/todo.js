a9fb577fd84bba427dbbbfb82fc077a6c1a99b7c
Andrew <andy@andycondon.com>
2017-11-26 23:46:17 +1300

    formatting

--- js/todo.js
+++ js/todo.js
@@ -8,27 +8,28 @@ $(function() {
     projectId: "wishlist-fur-uns",
     storageBucket: "",
     messagingSenderId: "835879506683"
-  };
-  firebase.initializeApp(config);
+  }
+  firebase.initializeApp(config)
 
-  var database = firebase.database();
-  
+  var database = firebase.database()
 
-  var $newItemForm = $('#newItemForm');
-  var $textInput = $('input:text');
-  var $form = $('form');
-  var emptyItemText = '<p class="error">Heyyyy! No empty items on the list please.</p>';
-  var fiveItemsText = '<p class="error">Ooooo. That\'s five items. Niiice.</p>';
-  var $fiveMsg = $($.parseHTML(fiveItemsText));
-  var $errorMsg = $($.parseHTML(emptyItemText));
-  var $listItems = $('li');
-  var $listItemText = $('li a').text;
+
+  var $newItemForm = $('#newItemForm')
+  var $textInput = $('input:text')
+  var $form = $('form')
+  var emptyItemText = '<p class="error">Heyyyy! No empty items on the list please.</p>'
+  var fiveItemsText = '<p class="error">Ooooo. That\'s five items. Niiice.</p>'
+  var $fiveMsg = $($.parseHTML(fiveItemsText))
+  var $errorMsg = $($.parseHTML(emptyItemText))
+  var $listItems = $('li')
+  var $listItemText = $('li a').text
   var todosArray = []
-
+  var entryToBeUpdated, entryText
 
   database.ref('list').on('value', function(snapshot) {
-    // snapshot.val() gives the current state of the db
+    // hide loading icon and insult
     $load.hide()
+    // snapshot.val() gives the current state of the db
     displayItems(snapshot.val())
   })
 
@@ -36,18 +37,19 @@ $(function() {
     // clear the ul of li's
     $('ul').empty()
 
+    // generate li's for each list item
     for (var key in allTodos) {
-      $('ul').prepend('<li class="todo-item"><a id="' + allTodos[key].id + '" href="#">x</a><span>' + allTodos[key].item + '</span></li>');
+      $('ul').prepend('<li class="todo-item"><a id="' + allTodos[key].id + '" href="#">x</a><span>' + allTodos[key].item + '</span></li>')
     }
   }
 
   function addItem() {
     // Add new item to list
     $newItemForm.on('submit', function(e) {
-      e.preventDefault();
+      e.preventDefault()
       if ($textInput.val() !== '') {
 
-        var newText = $textInput.val();
+        var newText = $textInput.val()
 
         if (validUrl(newText)) {
           newText = '<a href="' + newText + '">' + newText + '</a>'
@@ -57,53 +59,53 @@ $(function() {
         todosArray.push(newText)
 
         // Clear text field
-        $textInput.val('');
+        $textInput.val('')
 
         addItemToDb(newText)
       } else {
-        var $newMsg = $($errorMsg).hide().fadeIn(1000);
-        $form.prepend($newMsg);
-      };
+        var $newMsg = $($errorMsg).hide().fadeIn(1000)
+        $form.prepend($newMsg)
+      }
 
       // Surprise five list items message
       if ($('li').length == 5) {
-        $form.prepend($fiveMsg.hide().fadeIn(800).delay(1000).fadeOut(500));
+        $form.prepend($fiveMsg.hide().fadeIn(800).delay(1000).fadeOut(500))
       }
 
       if ($('li').length >= 1) {
-        $('ul').css("margin-bottom", "5%");
+        $('ul').css("margin-bottom", "5%")
       }
-    });
+    })
 
     // Make 'x's remove list item when clicked
     $('ul').on('click', '.todo-item>a', function(e) {
-      e.preventDefault();
-      $(this).parent().fadeOut(400);
+      e.preventDefault()
+      $(this).parent().fadeOut(400)
       database.ref('list/' + e.target.id).remove()
       console.log(e.target.id)
-    });
+    })
 
-  };
-  addItem();
+  }
+  addItem()
 
   function addItemToDb(newListItem) {
     // Get a key for a new Post.
-    var newPostKey = firebase.database().ref().child('posts').push().key;
+    var newPostKey = firebase.database().ref().child('posts').push().key
     var singleItem = {
       item: newListItem,
       id: newPostKey,
       order: todosArray.length
-    };
+    }
 
     database.ref('list/' + newPostKey).set(singleItem)
   }
 
   function validUrl(str) {
-    var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+    var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/
     if(!regex .test(str)) {
-      return false;
+      return false
     } else {
-      return true;
+      return true
     }
   }
 
@@ -122,9 +124,9 @@ $(function() {
     console.log(listValues)
   }
 
-  // $("ul").sortable({change: sortEventHandler, placeholder: 'placeholder'});
+  // $("ul").sortable({change: sortEventHandler, placeholder: 'placeholder'})
 
-  // $("ul").disableSelection();
+  // $("ul").disableSelection()
 
   // Refresh button
   $('.refresh').click(function(e) {
@@ -132,4 +134,4 @@ $(function() {
     location.reload(true)
   })
 
-});
+})
